37f31d5ac01762199b39043cf0dbb8d3e5dc5b98
koyu <me@koyu.space>
2024-07-21 01:50:19 +0200

    some link fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import remarkExternalLinks from "remark-external-links";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,7 @@ export default defineConfig({
     }),
     react(),
   ],
+  markdown: {
+    remarkPlugins: [[remarkExternalLinks, { target: '_blank' }]],
+  },
 });

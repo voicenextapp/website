868bafb0bbc58ebb1ebb8e4dfe4768e548d220ea
koyu <me@koyu.space>
2025-06-20 20:35:06 +0200

    Fix SSR plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkExternalLinks from "remark-external-links";
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +13,9 @@ export default defineConfig({
     react(),
   ],
   output: "server",
-  adapter: netlify(),
+  adapter: node({
+    mode: 'standalone',
+  }),
   markdown: {
     remarkPlugins: [
       [
